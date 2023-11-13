b4a6c38ad805313c8564312827a40424844370f5
DeclannH <69803173+DeclannH@users.noreply.github.com>
2023-11-12 19:12:48 -0500

    Update add and push, item list display
    Updating an item list display to come from TS, working on the add and delete from list

--- groceries/src/app/tab1/tab1.page.ts
+++ groceries/src/app/tab1/tab1.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NavController, ToastController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +9,53 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+
+
+
+
+  
+constructor(public navCtrl: NavController, public toastCtrl: ToastController) {
+}
+
+items = [
+  "Milk",
+  "Bread",
+  "Eggs",
+  "Chickens **NO ROOSTERS**",
+  "Flagyl"
+
+]
+
+addItem(): void{
+  console.log("Adding Item");
+
+  
+}
+
+public addButtons = ['OK'];
+  public addInputs = [
+    {
+      placeholder: 'Grocery Item',
+    },
+
+  ];
+
+  public deleteButtons = ['OK'];
+  public deleteInputs = [
+    {
+      placeholder: 'Grocery Item',
+    },
+
+  ];
 
 }
+
+
+
+
+
+
+
+
+
+
